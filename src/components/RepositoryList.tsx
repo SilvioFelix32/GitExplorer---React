c30c70686d902543f5418e07e4047f30389630f5
Silvio Félix <silvio.felix32@hotmail.com>
2021-10-22 12:23:39 -0300

    firt commit

---
+++ src/components/RepositoryList.tsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import { RepositoryItem } from "./RepositoryItem";
+
+import '../styles/repositories.scss';
+
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
+}
+
+export function RepositoryList() {
+    const [repositories, setRepositories] = useState<Repository[]>([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/SilvioFelix32/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+            console.log(repositories)
+    }, []); //USE EFFECT SEMPRE TEM QUE TER UM SEGUNDO PARAMETRO, SE NÃO HOUVER, ELE ENTRA EM LOOP, E FICA EXECUTANDO INUMERAS VEZES //
+
+    return (
+        <section className="repository-list">
+            <h1>Lista de Repositórios</h1>
+
+            <ul>
+            {repositories.map(repository => {
+                return <RepositoryItem key={repository.name} 
+                    repository={repository}></RepositoryItem>
+            })}
+            
+            </ul>
+        </section>
+    );
+}
